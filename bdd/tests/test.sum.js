417a72bba9ff913a5481f273d34c704d2dd39c9a
andrewbaillie <andrewbaillie@me.com>
2018-07-04 15:57:08 +0100

    Added new tests

--- bdd/tests/test.sum.js
+++ bdd/tests/test.sum.js
@@ -1,5 +1,15 @@
 describe('sum', function () {
-  it('should return sum of arguments', function () {
-    chai.expect(sum(1, 2)).to.equal(3);
-  });
+
+    it('should return sum of arguments', function () {
+        chai.expect(sum(1, 2)).to.equal(3);
+    });
+
+    it('should return a number', function () {
+        chai.expect(sum(1, 2)).to.be.a('number');
+    });
+
+    it('should return a string', function () {
+        chai.expect(sum('a', 2)).to.be.a('string');
+    });
+
 });
